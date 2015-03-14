77f3cd43df3466c20ff6d514a3dd9cafed032f12
alexTenche <alex.tenche@gmail.com>
2015-03-14 20:05:35 +0200

    strings

---
+++ strings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt for and read in the gamertag
+            Console.Write("Enter gamertag: ");
+            string gamertag = Console.ReadLine();
+
+            // prompt and read in the level
+            Console.Write("Enter level: ");
+            int level = int.Parse(Console.ReadLine());
+
+            // extract the first character of gamertag
+            char firstGamertagCharacter = gamertag[0];
+
+            // print out values
+            Console.WriteLine();
+            Console.WriteLine("Gamertag: " + gamertag);
+            Console.WriteLine("Level: " + level);
+            Console.WriteLine("First gamertag character: " + firstGamertagCharacter);
+            Console.WriteLine();
+            
+        }
+    }
+}
---
+++ stringsOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read in csv string
+            Console.Write("Enter name and percent (name,percent): ");
+            string csvString = Console.ReadLine();
+
+            // find comma location
+            int commaLocation = csvString.IndexOf(',');
+
+            // extract name and percent
+            string name = csvString.Substring(0, commaLocation);
+            float percent = float.Parse(csvString.Substring(commaLocation + 1));
+
+            // print name and percent
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Percent: " + percent);
+        }
+    }
+}
