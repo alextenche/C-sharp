9e12b90d07657783defcaf9af55cd196beb7f4d4
alexTenche <alex.tenche@gmail.com>
2015-03-15 10:04:23 +0200

    working on week 3

---
+++ ifStatements/ifStatements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask for and get user answer
+            Console.Write("Pick up the shiny think ? (y/n): ");
+            char answer = Console.ReadLine()[0];
+
+            // print appropriate message
+            if (answer == 'y' || answer == 'Y')
+            {
+                Console.WriteLine("You have the shiny object !");
+            } else if(answer == 'n' || answer == 'N'){
+                Console.WriteLine("Nope !");
+            }
+            else
+            {
+                Console.WriteLine("Do not undestand what you want !");
+            }
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ switch/switch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask for and get user answer
+            Console.Write("Pick up the shiny think ? (y/n): ");
+            char answer = Console.ReadLine()[0];
+
+            // print appropriate message
+            switch (answer)
+            {
+                case 'y':
+                case 'Y':
+                    Console.WriteLine("You have the shiny object !");
+                    break;
+                case 'n':
+                case 'N':
+                    Console.WriteLine("Nope !");
+                    break;
+                default:
+                    Console.WriteLine("Do not undestand what you want !");
+                    break;
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
