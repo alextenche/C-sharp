b75121938c31b2530eb5ec84b688c5bfb345817a
alexTenche <alex.tenche@gmail.com>
2015-04-19 10:22:00 +0300

    week 9

---
+++ DieExample/DieExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieExample
+{   
+    /// <summary>
+    /// Demonstrates implementation of Die class
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Test the Die class
+        /// </summary>
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("J - Jump");
+            Console.WriteLine("C - Crouch");
+            Console.WriteLine("Q - Quit");
+            Console.WriteLine();
+
+            Console.Write("Enter choice: ");
+            char choice = char.Parse(Console.ReadLine().ToUpper());
+
+            // validate input
+            while(choice != 'J' && choice != 'C' && choice != 'Q')
+            {
+                Console.WriteLine();
+                Console.WriteLine("Invalid input");
+
+                Console.WriteLine("J - Jump");
+                Console.WriteLine("C - Crouch");
+                Console.WriteLine("Q - Quit");
+                Console.WriteLine();
+
+                Console.Write("Enter choice: ");
+                choice = char.Parse(Console.ReadLine().ToUpper());
+            }
+            Console.WriteLine();
+        }
+    }
+}
